2fac4bcb157910a26dd7ed3e4c37c56f5099cf9c
junggc <jung@kacelab.com>
2021-12-15 15:48:52 +0900

    구구단 작업 do while 까지

--- src/ppt/Prac4.java
+++ src/ppt/Prac4.java
@@ -3,20 +3,19 @@ package ppt;
 public class Prac4 {
 
     public static void main(String[] args) {
-        int i = 2, j= 1;
+        int i=2, j=1;
         do {
             do {
-                System.out.print(j + "x" + i + "=" + i * j+"\t");
+                System.out.print(j + "x" + i + "=" + i * j + "\t");
                 j++;
             } while (j <= 9);
-            i++;
-            j=1;
             System.out.println();
+                i++;
+                j=1;
+        } while (i <= 9) ;
 
-        }  while (i <= 9) ;
+
+
     }
-
 }
-
-
 
