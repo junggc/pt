6014a25a462bf27fc9787e2b665d0b6c8239fef6
junggc <jung@kacelab.com>
2021-12-15 18:34:42 +0900

    구구단 작업 do while 까지

--- src/ppt/Prac.java
+++ src/ppt/Prac.java
@@ -1,15 +1,40 @@
 package ppt;
 
 public class Prac {
-
     public static void main(String[] args) {
-        for(int i =2; i <=9; i++){
-            for(int j=1; j <=9; j++)
-            System.out.print(j+"x"+i+"="+i*j+"\t");
+        for (int i = 2; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++)
+                System.out.print(j + "x" + i + "=" + i * j + "\t");
             System.out.println();
         }
     }
-
-
 }
+
+
+//    public static void main(String[]args){
+//        int i=1;
+//
+//        for(i=1; i<=9; i++) {
+//                    int j=1;
+//            for (j=1; j<=9; j++) {
+//                System.out.print(j+ "x" + i + "=" + i * j + "\t");
+//
+//
+//            }System.out.println();
+//
+//
+//        }
+//
+//    }
+//}
+//
+//
+
+
+
+
+
+
+
+
 
