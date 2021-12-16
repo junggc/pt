3d44c98e99eca155e47dbe45fe6511b7722423ae
junggc <jung@kacelab.com>
2021-12-16 16:17:13 +0900

    가위바위보게임 작업중 반복안됨

--- src/ppt/Prac6.java
+++ src/ppt/Prac6.java
@@ -9,26 +9,23 @@ public class Prac6 {
     private static int vCount; //이긴 횟수 고유 변수
     private static int lCount; // 진 횟수 고유 변수
     private static int dCount; // 비긴 횟수 고유 변수
-    private static String gamePlay = "";
+
 
     public static void main(String[] args) {
 
         int vCount = 0; //이긴경우 vCount
         int lCount = 0; //진 경우 lCount
         int dCount = 0; //비긴경우 dCount
-        String gamePlay;
+
         int c;//c는 PC
         int d;//d는 나
-        while (true) {
-            Random rd = new Random();//rd
-            c = (int) (Math.random() * 3) + 1;//c는 컴퓨터 랜덤값
-            System.out.println(c);
-            //1=주먹 2=가위 3=보
-            Scanner sc = new Scanner(System.in); //내가 입력할 스캐너
-
-            System.out.println("1~3 사이의 숫자를 입력하시오");
-
-
+        Scanner sc = new Scanner(System.in); //내가 입력할 스캐너
+        while(true) {
+                Random rd = new Random();//rd
+                c = (int) (Math.random() * 3) + 1;//c는 컴퓨터 랜덤값
+                System.out.println(c);
+                //1=주먹 2=가위 3=보
+                System.out.println("1~3 사이의 숫자를 입력하시오");
                 d = sc.nextInt(); // 내가 입력하는부분
 
                 if (c == d) {
@@ -90,16 +87,8 @@ public class Prac6 {
                 System.out.println("이긴횟수:" + vCount);
                 System.out.println("진 횟수:" + lCount);
                 System.out.println("비긴 횟수:" + dCount);
-            System.out.println("계속 하시겠습니까? y or n 으로 입력하세요");
-            gamePlay = sc.nextLine();
-            if (gamePlay.equals("y")) {
-                continue;
-            } else if (gamePlay.equals("n")) {
-                break;
-            }
-
-            }
         }
 
     }
 
+}
