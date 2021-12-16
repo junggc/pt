0686cc58e12b27a078a38a475eca489a501fc4a1
jung.kacelab <jung@kacelab.com>
2021-12-16 19:37:53 +0900

    c,d 바뀜수정

--- src/ppt/Prac6.java
+++ src/ppt/Prac6.java
@@ -13,53 +13,52 @@ public class Prac6 {
 
     public static void main(String[] args) {
 
-        int vCount = 0; //이긴경우 vCount
-        int lCount = 0; //진 경우 lCount
+        int e = 0; //이긴경우 e
+        int f = 0; //진 경우 f
         int dCount = 0; //비긴경우 dCount
 
-        int c;//c는 PC
-        int d;//d는 나
+        int d;//c는 PC
+        int c;//d는 나
         Scanner sc = new Scanner(System.in); //내가 입력할 스캐너
-
-
+        System.out.println();
             while(true) {
                 Random rd = new Random();//rd
-                c = (int) (Math.random() * 3) + 1;//c는 컴퓨터 랜덤값
-//                System.out.println(c);
+                d = (int) (Math.random() * 3) + 1;//c는 컴퓨터 랜덤값
+                System.out.println(d);
                 //1=주먹 2=가위 3=보
                 System.out.println("1~3 사이의 숫자를 입력하시오");
-                d = sc.nextInt(); // 내가 입력하는부분
+                c = sc.nextInt(); // 내가 입력하는부분
 
                 if (c == d) {
                     dCount++;
                     System.out.println("비겼습니다");
                 } else if (c == 1 && d == 2) {
-                    lCount++;
-                    System.out.println("졌습니다");
+                    e++;
+                    System.out.println("이겼습니다");
                 } else if (c == 1 && d == 3) {
-                    vCount++;
-                    System.out.println("이겼습니다");
+                    f++;
+                    System.out.println("졌습니다");
                 } else if (c == 2 && d == 1) {
-                    vCount++;
-                    System.out.println("이겼습니다");
+                    f++;
+                    System.out.println("졌습니다");
                 } else if (c == 2 && d == 3) {
-                    lCount++;
-                    System.out.println("졌습니다");
-                } else if (c == 3 && d == 1) {
-                    lCount++;
-                    System.out.println("졌습니다");
-                } else if (c == 3 && d == 2) {
-                    vCount++;
+                    e++;
                     System.out.println("이겼습니다");
+                } else if (c == 3 && d == 1) {
+                    e++;
+                    System.out.println("이겼습니다");
+                } else if (c == 3 && d == 2) {
+                    f++;
+                    System.out.println("졌습니다");
                 }
-                if (c == 1) {
+                if (d == 1) {
                     System.out.println("PC=주먹");
-                } else if (c == 2) {
+                } else if (d == 2) {
                     System.out.println("PC=가위");
                 } else {
                     System.out.println("PC=보");
                 }
-                switch (d) {
+                switch (c) {
                     case (1):
                         System.out.println("나=주먹");
                         break;
@@ -70,29 +69,30 @@ public class Prac6 {
                         System.out.println("나=보");
                         break;
                 }
-                if (c == 1 && d == 2) {
+                if (d == 1 && c == 2) {
+                    System.out.println(t22.a);
+                } else if (d == 2 && c == 1) {
                     System.out.println(t22.b);
-                } else if (c == 2 && d == 1) {
-                    System.out.println(t22.a);
-                } else if (c == 3 && d == 2) {
-                    System.out.println(t22.a);
+                } else if (d == 3 && c == 2) {
+                    System.out.println(t22.b);
                 }
-                if (c == 1 && d == 3) {
+                if (d == 1 && c == 3) {
+                    System.out.println(t22.b);
+                } else if (d == 2 && c == 3) {
                     System.out.println(t22.a);
-                } else if (c == 2 && d == 3) {
-                    System.out.println(t22.b);
-                } else if (c == 3 && d == 1) {
-                    System.out.println(t22.b);
+                } else if (d == 3 && c == 1) {
+                    System.out.println(t22.a);
                 } else if (c == d) {
                     System.out.println(t22.g);
                 }
-                System.out.println("이긴횟수:" + vCount);
-                System.out.println("진 횟수:" + lCount);
+                System.out.println("이긴횟수:" + e);
+                System.out.println("진 횟수:" + f);
                 System.out.println("비긴 횟수:" + dCount);
-                System.out.println("계속 게임을 진행 합니까? y or n");
+//                System.out.println("계속 게임을 진행 합니까? y or n");
+                Scanner g = new Scanner(System.in);
+//                g = sc.nextLine();
+//                System.out.println();
 
-                g = sc.nextLine();
-                System.out.println();
             }
 
 
