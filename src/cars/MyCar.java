13a4aad897f854931f3cfb2e627602474c709acf
kacelab <jung@kacelab.com>
2022-01-10 17:45:19 +0900

    게터세터 막 해보기

--- src/cars/MyCar.java
+++ src/cars/MyCar.java
@@ -15,19 +15,19 @@ public class MyCar {
         Scanner sc = new Scanner(System.in);
         System.out.println("브랜드 이름을 입력하세요");
         carinfo.setCarBrand(sc.nextLine());
-
+        carinfo.setCarName("달구지");
         if(carinfo.getCarBrand().equals("현대")){
             System.out.println(carinfo.getCarBrand());
         }else if(carinfo.getCarType().equals("Truck")){
             System.out.println("이 차의 종류는 트럭 입니다.");
         }else{
-            System.out.println("이 차의 종류는 세단 입니다.");
+            System.out.println("이 차의 종류는 분류되지 않았습니다.");
         }
 
 
 
 
-        System.out.println("제 차는 "+carinfo.getCarYear()+" 년식 "+carinfo.getCarBrand()+" "+carinfo.getCarName()+" "+ "이며"+" 색상은 "+carinfo.getCarColor()+" "+"입니다.");
+        System.out.println("이 차는 "+carinfo.getCarYear()+" 년식 "+carinfo.getCarBrand()+" "+carinfo.getCarName()+" "+ "이며"+" 색상은 "+carinfo.getCarColor()+" "+"입니다.");
 
 
 
