e7ba494f889405b1416b6d168fd3b0b20c3af0e2
kacelab <jung@kacelab.com>
2022-01-10 15:02:37 +0900

    게터세터 막 해보기

--- src/cars/Carinfo.java
+++ src/cars/Carinfo.java
@@ -2,20 +2,54 @@ package cars;
 
 public class Carinfo {
 
+    private String carColor = "black";
+    private String carName = "G70";
+    private String carType = "compactSedan";
+    private int carYear = 2020;
+    private String carBrand = "GENESIS";
 
-
-        private String carColor = "black";
-        private String carName = "G70";
-        private String carType = "compactSedan";
-        private int carYear = 2020;
-        private String carBrand = "GENESIS";
-
-        public void setCarColor(String carColor){
-            this.carColor = carColor;
-//        return "차색깔은 "+this.carColor+" 입니다.";
-        }
-
+    public String getCarColor() {
+        return carColor;
     }
+
+    public void setCarColor(String carColor) {
+        this.carColor = carColor;
+    }
+
+    public String getCarName() {
+        return carName;
+    }
+
+    public void setCarName(String carName) {
+        this.carName = carName;
+    }
+
+    public String getCarType() {
+        return carType;
+    }
+
+    public void setCarType(String carType) {
+        this.carType = carType;
+    }
+
+    public int getCarYear() {
+        return carYear;
+    }
+
+    public void setCarYear(int carYear) {
+        this.carYear = carYear;
+    }
+
+    public String getCarBrand() {
+        return carBrand;
+    }
+
+    public void setCarBrand(String carBrand) {
+        this.carBrand = carBrand;
+    }
+}
+
+
 
 
 
--- src/cars/MyCar.java
+++ src/cars/MyCar.java
@@ -1,14 +1,49 @@
 package cars;
 
+import java.util.Scanner;
+
 public class MyCar {
-
-    public MyCar() {
-    }
-
     public static void main(String[] args) {
 
+        Carinfo carinfo = new Carinfo();
+
+        carinfo.setCarBrand("대우");
+        carinfo.setCarYear(1980);
+        carinfo.setCarColor(String.valueOf(123456));
+        carinfo.setCarName("");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("브랜드 이름을 입력하세요");
+        carinfo.setCarBrand(sc.nextLine());
+
+        if(carinfo.getCarBrand().equals("현대")){
+            System.out.println(carinfo.getCarBrand());
+        }else if(carinfo.getCarType().equals("Truck")){
+            System.out.println("이 차의 종류는 트럭 입니다.");
+        }else{
+            System.out.println("이 차의 종류는 세단 입니다.");
+        }
+
+
+
+
+        System.out.println("제 차는 "+carinfo.getCarYear()+" 년식 "+carinfo.getCarBrand()+" "+carinfo.getCarName()+" "+ "이며"+" 색상은 "+carinfo.getCarColor()+" "+"입니다.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
-
 }
